c3c8254fe12ba4e524b7c7a151412f79273008b4
Christophe Malo <cmalo@reseau-delta.ch>
2020-07-23 23:44:53 +0200

    feat(exercice1): met en place une liaison sur un évènement

--- src/app/exercice1/exercice1.component.ts
+++ src/app/exercice1/exercice1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { preserveWhitespacesDefault } from '@angular/compiler';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-exercice1',
@@ -9,8 +11,21 @@ export class Exercice1Component implements OnInit {
 
   // Attribut du checkbox
   type = 'checkbox';
+  id = 'checkbox-liaison'
   name = 'checkbox';
   checked = true;
+  fontSize = '10px';
+
+
+  /**
+   * @description
+   * Modifie la taille de la police
+   * 
+   * @param size la taille de la font
+   */
+  public changeFontSize(size: string): void {
+    this.fontSize = size;
+  }
 
   constructor() { }
 
