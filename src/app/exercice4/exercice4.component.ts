e0e9da1d0fd452131a47809c7db38d037ab300b9
Christophe Malo <cmalo@reseau-delta.ch>
2020-07-24 23:12:29 +0200

    feat(exercice4): affiche les éléments du tableau texts

--- src/app/exercice4/exercice4.component.ts
+++ src/app/exercice4/exercice4.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice4Component implements OnInit {
 
+  public texts: string[] = ['un', 'deux', 'trois'];
+
   constructor() { }
 
   ngOnInit(): void {
